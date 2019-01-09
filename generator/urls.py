3e0b405bb87c5cf2d9ac408a444fda1048ce8de3
Sharp, Valerie <vsharp2@thezenith.com>
2019-01-09 15:48:23 -0800

    removed debug code

--- generator/urls.py
+++ generator/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = 'generator'
 urlpatterns = [
-    # path('', views.IndexView.as_view(), name='index')
     path('', views.index, name='index'),
     path('mood/', views.generate_mood, name='mood')
 ]
